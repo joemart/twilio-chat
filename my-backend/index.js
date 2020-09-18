374927e43a3b64e96fee3e98bd23082130e04448
Joseph Martire <joemart06@gmail.com>
2020-09-18 12:20:11 -0400

    Push functioning channels. Chat isn't displaying properly.

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,2 +1,36 @@
 require('dotenv-flow').config({node_env:"local"})
-console.log(process.env.TEST)
+
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Twilio = require('twilio')
+
+
+const AccessToken = Twilio.jwt.AccessToken
+const ChatGrant = AccessToken.ChatGrant
+
+app.use(cors())
+
+app.get('/token/:identity', function (req, res) {
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+  )
+
+  token.identity = req.params.identity
+  token.addGrant(new ChatGrant({
+    serviceSid: process.env.TWILIO_CHAT_SERVICE_SID
+  }))
+
+  res.set('Content-Type', 'application/json')
+  res.send({
+    identity: token.identity,
+    jwt: token.toJwt()
+  })
+})
+
+app.listen(process.env.PORT, function () {
+  console.log('Programmable Chat token server listening on port 3001!')
+})
